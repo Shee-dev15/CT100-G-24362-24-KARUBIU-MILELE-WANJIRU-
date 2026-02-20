aebe87ebd1765ff2c81fa550e38166fa688a2994
Shee-dev15 <wanjirumilele15@gmail.com>
2026-02-20 15:24:15 +0300

    Create units.java
    WEEK ONE JAVA ASSIGNMENT CT100/G/24362/24

---
+++ units.java
@@ -0,0 +1,22 @@
+/*
+ * Registration number: CT100/G/24362/24
+ * NAME: KARUBIU MILELE WANJIRU
+ * Program: Units.java
+ * Purpose: Displays a formatted list of academic units offered this semester.
+ */
+public class Units {
+
+    public static void main(String[] args) {
+        
+        // Print the header for the list
+        System.out.println("Units Offered This Semester:");
+        
+        // Print each unit on a new line using System.out.println
+        System.out.println("1. Introduction to Programming");
+        System.out.println("2. Data Structures");
+        System.out.println("3. Computer Networks");
+        System.out.println("4. Database Systems");
+        System.out.println("5. Software Engineering");
+        
+    }
+}
